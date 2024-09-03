4950086bb55335d1642eddb839a9a2ea6db6984d
zmusaddique <musaddique092@gmail.com>
2024-09-03 13:24:12 +0530

    First config
    The initial setup for neovim

---
+++ init.lua
@@ -0,0 +1,65 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+require("config.lazy")
+require("config.telescope")
+require("config.neotree")
+
+-- vim.cmd("colorscheme nightfox")
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+vim.opt.smartindent = true
+
+-- backup files and undotree
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- only incremental search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.cursorline = true
+-- vim.opt.cursorcolumn = true
+
+-- update time
+vim.opt.updatetime = 50
+
+-- vim.opt.colorcolumn = "80"
+
+-- show line numbers
+vim.wo.number = true
+
+-- show relative line numbers
+vim.wo.relativenumber = true
+
+-- move
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,4 @@
+return {
+  "mbbill/undotree",
+  vim.keymap.set('n', "<leader>u", vim.cmd.UndotreeToggle)
+}
